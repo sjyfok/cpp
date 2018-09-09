d205f85fffa7b0da4055f78753c05c83b5fac8c2
songjiayu <songjyf@126.coom>
2018-09-09 22:19:37 +0800

    鼠标捕获

---
+++ Source/MouseCap/MouseCap.h
@@ -0,0 +1,32 @@
+#ifndef MOUSECAP_MFC_H__
+#define	MOUSECAP_MFC_H__
+
+#include <afxwin.h>
+class CMyApp : public CWinApp
+{
+public:
+	virtual BOOL InitInstance();
+};
+
+class CMainWindow : public CFrameWnd
+{
+protected:
+	BOOL m_bTracking;
+	BOOL m_bCaptureEnabled;
+	CPoint m_ptFrom;
+	CPoint m_ptTo;
+
+	void InvertLine(CDC* pDC, CPoint ptFrom, CPoint ptTo);
+public:
+	CMainWindow();
+protected:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnNcLButtonDown(UINT nHitTest, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+
+
+#endif
