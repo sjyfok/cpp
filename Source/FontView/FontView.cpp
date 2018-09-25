dbf0240bcc536a707ed33c49028784c0f6a222f7
songjiayu <songjyf@126.com>
2018-09-25 14:04:55 +0800

    新增FontView文件

---
+++ Source/FontView/FontView.cpp
@@ -0,0 +1,29 @@
+#include <afxwin.h>
+#include "FontView.h"
+
+CMyApp myApp;
+
+BOOL CMyApp::InitInstance()
+{
+	m_pMainWnd = new CMainWindow;
+	m_pMainWnd->ShowWindow(m_nCmdShow);
+	m_pMainWnd->UpdateWindow();
+	return true;
+}
+
+BEGIN_MESSAGE_MAP(CMainWindow, CFrameWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+CMainWindow::CMainWindow()
+{
+	Create(NULL, _T("The Hello Application"));
+}
+
+void CMainWindow::OnPaint()
+{
+	CPaintDC dc(this);
+	CRect Rect;
+	GetClientRect(&Rect);
+	dc.DrawText(_T("Hello MFC"), -1, &Rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+}
