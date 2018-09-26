1224e152c300f93d52cc388eff65d2df5fc8a9a3
songjiayu <songjyf@126.com>
2018-09-26 08:42:06 +0800

    FontView 头文件和源文件被修改

--- Source/FontView/FontView.h
+++ Source/FontView/FontView.h
@@ -10,12 +10,34 @@ private:
 
 };
 
-class CMainWindow : public CFrameWnd
+class CMainWindow : public CWnd
 {
+protected:
+	int m_cxChar;
+	int m_cyChar;
+
+	CFont m_fontMain;
+	CFont m_fontSample;
+
+	CStatic m_wndLBTitle;
+	CListBox m_wndListBox;
+	CButton m_wndCheckBox;
+	CButton m_wndGroupBox;
+	CStatic m_wndSampleText;
+	CButton m_wndPushButton;
+
+	void FillListBox();
 public:
 	CMainWindow();
+	static int CALLBACK EnumFontFamProc(ENUMLOGFONT *lpelf,
+		NEWTEXTMETRIC *lpntm, int nFontType, LPARAM lparam);
 protected:
-	afx_msg void OnPaint();
+	virtual void PostNcDestroy();
+	
+	afx_msg int OnCreate(LPCREATESTRUCT lpcs);
+	afx_msg void OnPushButtonClicked();
+	afx_msg void OnCheckBoxClicked();
+	afx_msg void OnSelChange();
 	DECLARE_MESSAGE_MAP()
 };
 
