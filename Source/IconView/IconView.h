381074194ed733ab3076b44e5c7fec65361edee0
songjiayu <songjyf@126.com>
2018-09-28 14:08:51 +0800

    增加IconView程序

---
+++ Source/IconView/IconView.h
@@ -0,0 +1,51 @@
+#ifndef ICONVIEW_MFC_H__
+#define	ICONVIEW_MFC_H__
+
+#include <afxwin.h>
+class CMyApp : public CWinApp
+{
+public:
+	virtual BOOL InitInstance();
+private:
+
+};
+
+class CIconListBox : public CListBox
+{
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT &cs);
+	virtual void MeasureItem(LPMEASUREITEMSTRUCT lpmis);
+	virtual void DrawItem(LPDRAWITEMSTRUCT lpdis);
+	int AddIcon(HICON hIcon);
+	void ProjectImage(CDC *pDc, LPRECT pRect, COLORREF clrBackColor);
+};
+
+class CMainWindow : public CWnd
+{
+protected:
+	int m_cxChar;
+	int m_cyChar;
+
+	CFont m_font;
+	CRect m_rcImage;
+
+	CButton m_wndGroupBox;
+	CIconListBox m_wndIconListBox;
+	CStatic m_wndLabel;
+public:
+	CMainWindow();
+protected:
+	virtual void PostNcDestroy();
+	
+	afx_msg int OnCreate(LPCREATESTRUCT lpcs);
+	afx_msg void OnPaint();
+	afx_msg void OnSetFocus(CWnd *pWnd);
+	afx_msg void OnDropFiles(HDROP hDropInfo);
+	afx_msg void OnSelChange();
+
+	DECLARE_MESSAGE_MAP()
+};
+
+
+
+#endif
