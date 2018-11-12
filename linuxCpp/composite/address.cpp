85dc46cdccd638bd5ac4515a15404f9365e0704f
songjiayu <songjyf@126.com>
2018-11-12 17:24:43 +0800

    增加新文件

---
+++ linuxCpp/composite/address.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int donuts = 6;
+    double cups = 4.5;
+
+    cout << "donuts value = " << donuts;
+    cout << " and donuts address = " << &donuts << endl;
+    cout << "cups value = " << cups;
+    cout << " and cups address = " << &cups << endl;
+
+    return 0;
+}
