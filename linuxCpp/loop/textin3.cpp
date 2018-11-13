9bedf3350f1fd66a8758874e1ff45d20bf2adafd
song <songjyf@126.com>
2018-11-13 21:14:40 +0800

    增加新文件

---
+++ linuxCpp/loop/textin3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char ch;
+    int count = 0;
+
+    cin.get(ch);
+    while(cin.fail() == false)
+    {
+        cout << ch;
+        ++ count;
+        cin.get(ch);
+    }
+    cout << endl << count << " characters read\n";
+    return 0;
+}
---
+++ linuxCpp/loop/textin4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char ch;
+    int count = 0;
+
+    while((ch = cin.get()) != EOF)
+    {
+        cout.put(char(ch));
+        ++ count;
+    }
+
+    cout << endl << count << " characters read\n";
+
+    return 0;
+}
