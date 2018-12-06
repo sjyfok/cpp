399fec6b962990155a157eb1cb2de7e10aef5e46
songjiayu <songjyf@126.com>
2018-12-06 18:00:37 +0800

    增加文件

--- linuxCpp/inherit/brass2/brass.cpp
+++ linuxCpp/inherit/brass2/brass.cpp
@@ -34,7 +34,68 @@ AcctABC::Formatting AcctABC::SetFormat() const
     return f;
 }
 
-void Acct
+void AcctABC::Restore(Formatting &f) const
+{
+    cout.setf(f.flag, ios_base::floatfield);
+    cout.precision(f.pr);
+}
+
+
+void Brass::Withdraw(double amt)
+{
+    //format initialState = setFormat();
+    //precis prec = cout.precision(2);
+
+    if (amt < 0)
+        cout << "Withrawal amount must be positive; "
+            << "withdrawal canceled.\n";
+    else if (amt <= Balance())
+        AcctABC::Withdraw(amt);
+    else
+        cout << "Withdrawal amount of $" << amt
+            << " exceeds your balance.\n"
+            << "Withdrawal canceled.\n";
+}
+
+void Brass::ViewAcct() const
+{
+//    format initialState = setFormat();
+    Formatting f = SetFormat();
+//    precis prec = cout.precision(2);
+    cout << "Brass Client: " << FullName() << endl;
+    cout << "Account Number: " << AcctNum() << endl;
+    cout << "Balance: $" << Balance() << endl;
+    Restore(f);
+}
+
+BrassPlus::BrassPlus(const string &s, long an, double bal,
+        double ml, double r):AcctABC(ba)
+{
+    maxLoan = ml;
+    owesBank = 0.0;
+    rate = r;
+}
+
+BrassPlus::BrassPlus(const Brass &ba, double ml, double r)
+    : AcctABC(ba)
+{
+    maxLoan = ml;
+    owesBank = 0.0;
+    rate = r;
+}
+
+void BrassPlus::ViewAcct() const
+{
+    Formatting f = SetFormat();
+
+    cout << "BrassPls Client: " << FullName() << endl;
+    cout << "Account Number: " << AcctNum() << endl;
+    cout << "Balance: $" << maxLoan << endl;
+    cout << "Maximum loan: $" << owesBank << endl;
+    cout.precision(3);
+    cout << "Loan Rate: " << 100*rate << "%\n";
+    Restore(f);
+}
 
 Brass::Brass(const string &s, long an, double bal)
 {
@@ -52,66 +113,15 @@ void Brass::Deposit(double amt)
         balance += amt;
 }
 
-void Brass::Withdraw(double amt)
-{
-    format initialState = setFormat();
-    precis prec = cout.precision(2);
-
-    if (amt < 0)
-        cout << "Withrawal amount must be positive; "
-            << "withdrawal canceled.\n";
-    else if (amt <= balance)
-        balance -= amt;
-    else
-        cout << "Withdrawal amount of $" << amt
-            << " exceeds your balance.\n"
-            << "Withdrawal canceled.\n";
-    restore(initialState, prec);
-}
 
 double Brass::Balance() const
 {
     return balance;
 }
 
-void Brass::ViewAcct() const
-{
-    format initialState = setFormat();
-    precis prec = cout.precision(2);
-    cout << "Client: " << fullName << endl;
-    cout << "Account Number: " << acctNum << endl;
-    cout << "Balance: $" << balance << endl;
-    restore(initialState, prec);
-}
 
-BrassPlus::BrassPlus(const string &s, long an, double bal,
-        double ml, double r):Brass(s, an, bal)
-{
-    maxLoan = ml;
-    owesBank = 0.0;
-    rate = r;
-}
 
-BrassPlus::BrassPlus(const Brass &ba, double ml, double r)
-    : Brass(ba)
-{
-    maxLoan = ml;
-    owesBank = 0.0;
-    rate = r;
-}
 
-void BrassPlus::ViewAcct() const
-{
-    format initialState = setFormat();
-    precis prec = cout.precision(2);
-
-    Brass::ViewAcct();
-    cout << "Maximum loan: $" << maxLoan << endl;
-    cout << "Owed to bank: $" << owesBank << endl;
-    cout.precision(3);
-    cout << "Loan Rate: " << 100*rate << "%\n";
-    restore(initialState, prec);
-}
 
 void BrassPlus::Withdraw(double amt)
 {
