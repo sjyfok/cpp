801bbb010a0455ca4fabc574581f1bbb6ea64f29
songjiayu <songjyf@126.com>
2019-01-18 14:33:07 +0800

    UseFrame有问题，不能运行，动态创建方面理解不清楚，需要在DynClass目录做测试

--- ConsoleSource/UseFrame/UseFrame.cpp
+++ ConsoleSource/UseFrame/UseFrame.cpp
@@ -1,23 +1,20 @@
-/////////////////////////////////////////
-// UseFrame.cpp文件
-
+#include <iostream>
+#include <Windows.h>
 #include "UseFrame.h"
 
+CMyApp theApp;
 
-CMyApp theApp;		// 应用程序实例对象
+using namespace std;
 
-///////////////////////////////////////
-// CMyApp类
 
 BOOL CMyApp::InitInstance()
 {
-	::MessageBox(NULL, "主线程开始执行！", "CMyApp::InitInstance", 0);
-
-	return FALSE;	// 不要进入消息循环
+	::MessageBox(NULL, "主线程开始执行!", "CMyApp::InitInstance", 0);
+	return FALSE;
 }
 
 int CMyApp::ExitInstance()
 {
-	::MessageBox(NULL, "主线程将要退出！", "CMyApp::ExitInstance", MB_OK);
+	::MessageBox(NULL, "主线程将要退出!", "CMyApp::ExitInstance", MB_OK);
 	return 0;
 }
