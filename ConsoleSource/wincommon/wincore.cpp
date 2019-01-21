9011518ad5d0c3292e8f86a14f7b00d6c13688d5
songjiayu <songjyf@126.com>
2019-01-21 13:25:31 +0800

    修改wincore.cpp文文件

--- ConsoleSource/wincommon/wincore.cpp
+++ ConsoleSource/wincommon/wincore.cpp
@@ -104,3 +104,77 @@ LRESULT CWnd::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
 {
 	return 0;
 }
+
+BOOL AfxEndDeferRegisterClass(LONG fToRegister)
+{
+	WNDCLASS wndclass;
+	memset(&wndclass, 0, sizeof(wndclass));
+	wndclass.lpfnWndProc = ::DefWindowProc;
+	wndclass.hInstance = AfxGetModuleState()->m_hCurrentInstanceHandle;
+	wndclass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	BOOL bResult = FALSE;
+	if (fToRegister & AFX_WND_REG)
+	{
+		wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+		wndclass.lpszClassName = _afxWnd;
+		bResult = AfxRegisterClass(&wndclass);
+	}
+	else if (fToRegister &AFX_WNDFRAMEORVIEW_REG)
+	{
+		wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+		wndclass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+		wndclass.lpszClassName = _afxWndFrameOrView;
+		bResult = AfxRegisterClass(&wndclass);
+	}
+	return bResult;
+}
+
+BOOL AfxRegisterClass(WNDCLASS *lpWndClass)
+{
+	WNDCLASS wndclass;
+	if (GetClassInfo(lpWndClass->hInstance, lpWndClass->lpszClassName, &wndclass))
+	{
+		return TRUE;
+	}
+	if (!::RegisterClass(lpWndClass))
+	{
+		TRACE("Can't register window class name %s\n", lpWndClass->lpszClassName);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+LPCTSTR AfxRegisterWndClass(UINT nClassStyle, HCURSOR hCursor,
+	HBRUSH hbtBackground, HICON hIcon)
+{
+	LPTSTR lpszName = AfxGetThreadState()->m_szTempClassName;
+	HINSTANCE hInst = AfxGetModuleState()->m_hCurrentInstanceHandle;
+	if (hCursor == NULL && hbrBackground == NULL && hIcon = NULL)
+	{
+		wsprintf(lpszName, "Afx:%d:%d", (int)hInst, nClassStyle);
+	}
+	else
+	{
+		wsprintf(lpszName, "Afx:%d:%d:%d:%d", (int)hInst, nClassStyle,
+			(int)hCursor, (int)hbtBackground, (int)hIcon);
+	}
+	WNDCLASS wc = { 0 };
+	if (::GetClassInfo(hInst, lpszName, &wc))
+	{
+		ASSERT(wc.style == nClassStyle);
+		return lpszName;
+	}
+	wc.hInstance = hInst;
+	wc.style = nClassStyle;
+	wc.hCursor = hCursor;
+	wc.hbrBackground = hbrBackground;
+	wc.hIcon = hIcon;
+	wc.lpszClassName = lpszName;
+	wc.lpfnWndProc = ::DefWindowProc;
+	if (!AfxRegisterClass(&wc))
+	{
+		TRACE("Can't register window class named %s\n", lpszName);
+		return NULL;
+	}
+	return lpszName;
+}	
