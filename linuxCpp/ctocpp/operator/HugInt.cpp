64844d4005a6d8cb74cf3e54363435e577f5757c
songjiayu <songjyf@126.com>
2019-08-05 16:58:39 +0800

    change

--- linuxCpp/ctocpp/operator/HugInt.cpp
+++ linuxCpp/ctocpp/operator/HugInt.cpp
@@ -1,31 +1,62 @@
 #include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
 using namespace std;
-class Point {
-private:
-	int x;
-	int y;
-public:
-	Point() { };
+const int MAX = 110;
+class CHugeInt {
 
 	// 在此处补充你的代码
-	friend istream& operator>>(istream& is, Point& pt)
+private:
+	char sNum[220];
+public:
+	CHugeInt(char *s)
 	{
-		is >> pt.x >> pt.y;
-		return is;
+		if (s != NULL)
+		{
+			strcpy(sNum, s);
+		}
+	}
+	CHugeInt(int n)
+	{
+		sprintf(sNum, "%d", n);
 	}
 
-	friend ostream& operator<<(ostream& os, Point pt)
+	char * operator+(CHugeInt a)
 	{
-		os << pt.x << "," << pt.y;
-		return os;
+		int len1, len2, len;
+		len1 = strlen(sNum);
+		len2 = strlen(a.sNum);
+		if (len1 > len2)
+		{
+			len = len2;
+		}
+		else
+		{
+			len = len1;
+		}
+		for (int i = 0; i < len; i++)
+		{
+
+		}
 	}
 };
-
-int main()
+int  main()
 {
-	Point p;
-	while (cin >> p) {
-		cout << p << endl;
+	char s[210];
+	int n;
+
+	while (cin >> s >> n) {
+		CHugeInt a(s);
+		CHugeInt b(n);
+
+		cout << a + b << endl;
+		cout << n + a << endl;
+		cout << a + n << endl;
+		b += n;
+		cout << ++b << endl;
+		cout << b++ << endl;
+		cout << b << endl;
 	}
 	return 0;
 }
