9bca726d0686af286f2693f8e2085b92d2860145
songjiayu <songjyf@126.com>
2019-09-05 11:26:41 +0800

    stl2 is done

--- linuxCpp/ctocpp/stl2/database.cpp
+++ linuxCpp/ctocpp/stl2/database.cpp
@@ -10,8 +10,47 @@ template <class Key, class T, class Pred = greater<Key>>
 class MyMultimap :public multimap<Key, T, Pred>
 {
 public:
+	void Set(Key k, T v)
+	{
+		typename multimap<Key, T, Pred>::iterator p;
+		p = this->begin();
+		while (p != this->end())
+		{
+			if (p->first == k)
+				p->second = v;
+			p++;
+		}
+	}
+	/*
+	void Set(string k, int v)
+	{
+		MyMultimap<string, int>::iterator p = this->begin();
+		while (p != this->end())
+		{
+			if (p->first == k)
+			p->second = v;
+			p++;
+		}
+	}
 
+	void Set(int k, string v)
+	{
+		MyMultimap<int, string>::iterator p = this->begin();
+		while (p != this->end())
+		{
+			if (p->first == k)
+				p->second = v;
+			p++;
+		}
+	}*/
 };
+
+template <class F, class S>
+ostream& operator<<(ostream& os, pair<F, S> &s)
+{
+	os << "(" << s.first << "," << s.second << ")";
+	return os;
+}
 
 struct Student
 {
@@ -33,45 +72,45 @@ int main()
 	MyMultimap<string, int> mp;
 	for (int i = 0; i<5; ++i)
 		mp.insert(make_pair(s[i].name, s[i].score));
-	//Print(mp.begin(), mp.end()); //按姓名从大到小输出
+	Print(mp.begin(), mp.end()); //按姓名从大到小输出
 
-	//mp.Set("Tom", 78); //把所有名为"Tom"的学生的成绩都设置为78
-	//Print(mp.begin(), mp.end());
+	mp.Set("Tom", 78); //把所有名为"Tom"的学生的成绩都设置为78
+	Print(mp.begin(), mp.end());
 
 
 
-	//MyMultimap<int, string, less<int> > mp2;
-	//for (int i = 0; i<5; ++i)
-	//	mp2.insert(make_pair(s[i].score, s[i].name));
+	MyMultimap<int, string, less<int> > mp2;
+	for (int i = 0; i<5; ++i)
+		mp2.insert(make_pair(s[i].score, s[i].name));
 
-	//Print(mp2.begin(), mp2.end()); //按成绩从小到大输出
-	//mp2.Set(70, "Error");          //把所有成绩为70的学生，名字都改为"Error"
-	//Print(mp2.begin(), mp2.end());
-	//cout << "******" << endl;
+	Print(mp2.begin(), mp2.end()); //按成绩从小到大输出
+	mp2.Set(70, "Error");          //把所有成绩为70的学生，名字都改为"Error"
+	Print(mp2.begin(), mp2.end());
+	cout << "******" << endl;
 
-	//mp.clear();
+	mp.clear();
 
-	//string name;
-	//string cmd;
-	//int score;
-	//while (cin >> cmd) {
-	//	if (cmd == "A") {
-	//		cin >> name >> score;
-	//		if (mp.find(name) != mp.end()) {
-	//			cout << "erroe" << endl;
-	//		}
-	//		mp.insert(make_pair(name, score));
-	//	}
-	//	else if (cmd == "Q") {
-	//		cin >> name;
-	//		MyMultimap<string, int>::iterator p = mp.find(name);
-	//		if (p != mp.end()) {
-	//			cout << p->second << endl;
-	//		}
-	//		else {
-	//			cout << "Not Found" << endl;
-	//		}
-	//	}
-	//}
+	string name;
+	string cmd;
+	int score;
+	while (cin >> cmd) {
+		if (cmd == "A") {
+			cin >> name >> score;
+			if (mp.find(name) != mp.end()) {
+				cout << "erroe" << endl;
+			}
+			mp.insert(make_pair(name, score));
+		}
+		else if (cmd == "Q") {
+			cin >> name;
+			MyMultimap<string, int>::iterator p = mp.find(name);
+			if (p != mp.end()) {
+				cout << p->second << endl;
+			}
+			else {
+				cout << "Not Found" << endl;
+			}
+		}
+	}
 	return 0;
 }
