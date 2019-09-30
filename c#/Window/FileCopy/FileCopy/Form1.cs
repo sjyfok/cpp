15f23326c8926f9f18dd314dae35bab970bf6481
songjiayu <songjyf@126.com>
2019-09-30 12:04:59 +0800

    new file

---
+++ c#/Window/FileCopy/FileCopy/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string szFileName = "test.txt";
+            string szFileNameCopy = "testcopy.txt";
+            if(!File.Exists(szFileName))
+            {
+                MessageBox.Show("文件不存在!");
+            }
+            else
+            {
+              
+                if (File.Exists(szFileNameCopy))
+                {
+                    File.Delete(szFileNameCopy);
+                }
+                File.Copy(szFileName, szFileNameCopy);
+                MessageBox.Show("文件复制成功!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string szFileNameCopy = "testcopy.txt";
+            if (File.Exists(szFileNameCopy))
+            {
+                File.Delete(szFileNameCopy);
+            }
+            File.CreateText(szFileNameCopy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string szFileNameCopy = "testcopy.txt";
+            if(File.Exists(szFileNameCopy))
+            {
+                File.Delete(szFileNameCopy);
+                MessageBox.Show("文件删除成功");
+            }
+        }
+    }
+}
