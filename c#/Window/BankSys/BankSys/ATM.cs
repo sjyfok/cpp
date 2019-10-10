85f67694f8de42916de5aeefe77fb833ad762919
songjiayu <songjyf@126.com>
2019-10-10 18:05:22 +0800

    new file

---
+++ c#/Window/BankSys/BankSys/ATM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+public class ATM
+{
+    Bank bank;
+
+    public ATM(Bank bank)
+    {
+        this.bank = bank;
+    }
+
+    public void Transaction()
+    {
+        Show("please insert your card");
+        string id = GetInput();
+        Show("please enter your password");
+        string pwd = GetInput();
+        Account account = bank.FindAccount(id, pwd);
+        if(account == null)
+        {
+            Show("card invalid or password no corrent");
+            return;
+        }
+        Show("1: display; 2: save; 3: withdraw");
+        string op = GetInput();
+        if (op == "1")
+        {
+            Show("balance: " + account.getMoney());
+        }
+        else if (op == "2")
+        {
+            Show("Save money");
+            string smoney = GetInput();
+            double money = double.Parse(smoney);
+            bool ok = account.SaveMoney(money);
+            if (ok) Show("ok");
+            else Show("error");
+            Show("balance: " + account.getMoney());
+        }
+        else if(op == "3")
+        {
+            Show("withdraw money");
+            string smoney = GetInput();
+            double money = double.Parse(smoney);
+            bool ok = account.WithdrawMoney(money);
+            if (ok) Show("ok");
+            else Show("error");
+
+            Show("balance:" + account.getMoney());
+        }
+    }
+
+    public void Show(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
+    public string GetInput()
+    {
+        return Console.ReadLine();
+    }
+}
