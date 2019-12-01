7027d4161394d8bcd068992f63aca48c1a66ec5a
songjyf <songjyf@126.com>
2019-12-01 21:13:37 +0800

    增加位图资源文件

--- c#/WinForm/mine/mine/Program.cs
+++ c#/WinForm/mine/mine/Program.cs
@@ -16,7 +16,7 @@ namespace mine
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
